9fdad52da6b86535457f39d23025d57f2a59e61f
Moisas <mopri08@gmail.com>
2022-07-23 02:24:01 -0300

    puted some products and was make a footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,141 @@
+const data = [
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Placa De Comando Cif Cervejeiro Imbera Ccv144 Ccv315 Ccv355',
+		imagens: [
+			{
+				img: '../img/products/Placa De Comando Cif Cervejeiro Imbera Ccv144 Ccv315 Ccv355/D_NQ_NP_655378-MLB50127685214_052022-O.webp',
+			},
+			{
+				img: '../img/products/Placa De Comando Cif Cervejeiro Imbera Ccv144 Ccv315 Ccv355/D_NQ_NP_951746-MLB50127583468_052022-O.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1667843309-placa-de-comando-cif-cervejeiro-imbera-ccv144-ccv315-ccv355-_JM',
+	},
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Placa De Comando Cif Cervejeiro Imbera Ccv144 Ccv315',
+		imagens: [
+			{
+				img: '../img/products/Placa De Comando Cif Cervejeiro Imbera Ccv144 Ccv315/D_NQ_NP_2X_655378-MLB50127685214_052022-F.webp',
+			},
+			{
+				img: '../img/products/Placa De Comando Cif Cervejeiro Imbera Ccv144 Ccv315/D_NQ_NP_2X_951746-MLB50127583468_052022-F.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1811888322-placa-de-comando-cif-cervejeiro-imbera-ccv144-ccv315-_JM',
+	},
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Controlador Display Tlb30 Cervejeiro Metalfrio',
+		imagens: [
+			{
+				img: '../img/products/Controlador Display Tlb30 Cervejeiro Metalfrio/D_NQ_NP_2X_722627-MLB46125855393_052021-F.webp',
+			},
+			{
+				img: '../img/products/Controlador Display Tlb30 Cervejeiro Metalfrio/D_NQ_NP_2X_837579-MLB46125833747_052021-F.webp',
+			},
+			{
+				img: '../img/products/Controlador Display Tlb30 Cervejeiro Metalfrio/D_NQ_NP_2X_852160-MLB46125824745_052021-F.webp',
+			},
+			{
+				img: '../img/products/Controlador Display Tlb30 Cervejeiro Metalfrio/D_NQ_NP_2X_951338-MLB46125846528_052021-F.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1734548051-controlador-display-tlb30-cervejeiro-metalfrio-_JM',
+	},
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Fonte Para Lampadas De Leds Imbera',
+		imagens: [
+			{
+				img: '../img/products/Fonte Para Lampadas De Leds Imbera/D_NQ_NP_2X_744748-MLB45030357957_022021-F.webp',
+			},
+			{
+				img: '../img/products/Fonte Para Lampadas De Leds Imbera/D_NQ_NP_2X_759765-MLB45030377014_022021-F.webp',
+			},
+			{
+				img: '../img/products/Fonte Para Lampadas De Leds Imbera/D_NQ_NP_2X_920025-MLB45030357981_022021-F.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1809616345-fonte-para-lampadas-de-leds-imbera-_JM',
+	},
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Borracha Gaxeta Gvr43-t Gelopar Yeti',
+		imagens: [
+			{
+				img: '../img/products/Borracha Gaxeta Gvr43-t Gelopar Yeti/D_NQ_NP_2X_852328-MLB44348533902_122020-F.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1816585132-borracha-gaxeta-gvr43-t-gelopar-yeti-_JM',
+	},
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Lâmpada Led Imbera Stylus Slim Vr08 Vrs08 Ccv144 220v 5.5w',
+		imagens: [
+			{
+				img: '../img/products/Lâmpada Led Imbera Stylus Slim Vr08 Vrs08 Ccv144 220v 5.5w/D_NQ_NP_2X_793864-MLB43190322046_082020-F.webp',
+			},
+			{
+				img: '../img/products/Lâmpada Led Imbera Stylus Slim Vr08 Vrs08 Ccv144 220v 5.5w/D_NQ_NP_2X_900324-MLB43190300924_082020-F.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1694298951-lmpada-led-imbera-stylus-slim-vr08-vrs08-ccv144-220v-55w-_JM',
+	},
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Auto-fechamento Barra De Torção Imbera',
+		imagens: [
+			{
+				img: '../img/products/Auto-fechamento Barra De Torção Imbera/D_NQ_NP_2X_614753-MLB43486422476_092020-F.webp',
+			},
+			{
+				img: '../img/products/Auto-fechamento Barra De Torção Imbera/D_NQ_NP_2X_822235-MLB43486429143_092020-F.webp',
+			},
+			{
+				img: '../img/products/Auto-fechamento Barra De Torção Imbera/D_NQ_NP_2X_920349-MLB43486427207_092020-F (1).webp',
+			},
+			{
+				img: '../img/products/Auto-fechamento Barra De Torção Imbera/D_NQ_NP_2X_920349-MLB43486427207_092020-F.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1795322491-auto-fechamento-barra-de-torco-imbera-_JM',
+	},
+	{
+		fabricante: 'IMBERA',
+		tipo: 'controler',
+		nomeProduto: 'Kit Controlador Placa De Comando Cif Imbera Refresqueiro',
+		imagens: [
+			{
+				img: '../img/products/Kit Controlador Placa De Comando Cif Imbera Refresqueiro/D_NQ_NP_2X_768791-MLB42976788702_082020-F.webp',
+			},
+			{
+				img: '../img/products/Kit Controlador Placa De Comando Cif Imbera Refresqueiro/D_NQ_NP_2X_936954-MLB42976801421_082020-F.webp',
+			},
+			{
+				img: '../img/products/Kit Controlador Placa De Comando Cif Imbera Refresqueiro/D_NQ_NP_2X_979290-MLB42976788665_082020-F.webp',
+			},
+		],
+		caminhoVenda:
+			'https://kwcoldrefrigeracao.mercadoshops.com.br/MLB-1615130746-kit-controlador-placa-de-comando-cif-imbera-refresqueiro-_JM',
+	},
+]
+
 const fabricante = [
 	{
 		fabricante: 'IMBERA xxx',
@@ -48,3 +186,60 @@ function createListaFabricantesItens() {
 }
 createListaFabricantesItens()
 createListFabricantes()
+
+const dadosProduto = document.getElementById('main')
+const imgage = document.getElementById('imgProdutos')
+
+function produtos() {
+	data.map((dt, index) => {
+		dadosProduto.innerHTML += `
+		<div class='card'>
+			<div class='card-body'>
+				<div
+					id='carouselExampleControls${index}'
+					class='carousel slide'
+					data-bs-ride='carousel'
+				>
+					<div class='carousel-inner' id='imgProdutos'>
+						${dt.imagens.map((d, index) => {
+							if (index === Number(0)) {
+								return `<div class='carousel-item active'>
+									<img src='${d.img}' class='d-block w-100' alt='...' />
+								</div>`
+							} else {
+								return `<div class='carousel-item'>
+									<img src='${d.img}' class='d-block w-100' alt='...' />
+								</div>`
+							}
+						})}
+					
+					</div>
+					<button
+						class='carousel-control-prev'
+						type='button'
+						data-bs-target='#carouselExampleControls${index}'
+						data-bs-slide='prev'
+					>
+						<span class='carousel-control-prev-icon' aria-hidden='true'></span>
+						<span class='visually-hidden'>Previous</span>
+					</button>
+					<button
+						class='carousel-control-next'
+						type='button'
+						data-bs-target='#carouselExampleControls${index}'
+						data-bs-slide='next'
+					>
+						<span class='carousel-control-next-icon' aria-hidden='true'></span>
+						<span class='visually-hidden'>Next</span>
+					</button>
+				</div>
+				<p>${dt.nomeProduto}</p> 
+				<a href='${
+					dt.caminhoVenda
+				}' target="_blank" class='btn btn-primary'>Ver produto</a>
+			</div>
+		</div>`
+	})
+}
+
+produtos()
